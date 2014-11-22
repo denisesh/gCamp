710dd91699209dbb43126fdd26c9c91580b4003a
denisesh <denisesh@gmail.com>
2014-11-22 13:32:13 -0800

    add FAQ, cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
   get 'terms' => 'welcome#terms'
+  get 'faq'   => 'welcome#faq'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
